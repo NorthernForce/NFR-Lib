7ddf1fa8e63f1513cf2697ad20bcd1d9be849fad
cml1010101 <cml1010101@gmail.com>
2023-07-27 22:12:53 -0400

    added documentation for swerve drive with joystick

--- src/main/java/org/northernforce/commands/NFRSwerveDriveWithJoystick.java
+++ src/main/java/org/northernforce/commands/NFRSwerveDriveWithJoystick.java
@@ -8,12 +8,25 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * Command responsible for driving a swerve drive via joystick inputs.
+ */
 public class NFRSwerveDriveWithJoystick extends CommandBase
 {
     protected final NFRSwerveDrive drive;
     protected final NFRSwerveModuleSetState[] setStateCommands;
     protected final DoubleSupplier xSupplier, ySupplier, thetaSupplier;
     protected final boolean optimize, fieldRelative;
+    /**
+     * Creates a new NFRSwerveDriveWithJoystick
+     * @param drive the swerve drive instance
+     * @param setStateCommands the list of commands that will be used to set the state of the swerve drive.
+     * @param xSupplier the x supplier for the ChassisSpeeds.
+     * @param ySupplier the y supplier for the ChassisSpeeds.
+     * @param thetaSupplier the theta supplier for the ChassisSpeeds.
+     * @param optimize whether to optimize swerve module positions.
+     * @param fieldRelative whether to use field relative driving (recommended if gyro works).
+     */
     public NFRSwerveDriveWithJoystick(NFRSwerveDrive drive, NFRSwerveModuleSetState[] setStateCommands,
         DoubleSupplier xSupplier, DoubleSupplier ySupplier, DoubleSupplier thetaSupplier, boolean optimize,
         boolean fieldRelative)
@@ -27,6 +40,9 @@ public class NFRSwerveDriveWithJoystick extends CommandBase
         this.optimize = optimize;
         this.fieldRelative = fieldRelative;
     }
+    /**
+     * Initializes the command and schedules the swerve module set state commands.
+     */
     @Override
     public void initialize()
     {
@@ -35,6 +51,9 @@ public class NFRSwerveDriveWithJoystick extends CommandBase
             command.schedule();
         }
     }
+    /**
+     * Calculates inverse kinematics based on the supplied joystick inputs and feeds it to the setState commands.
+     */
     @Override
     public void execute()
     {
