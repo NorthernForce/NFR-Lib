8ca221e6659adafefb2f2d2351553923b75c149d
cml1010101 <cml1010101@gmail.com>
2023-07-27 20:34:17 -0400

    applied deadbands, reorganized, got rid of unnecessary logging, reset navx, and inverted right side

--- src/test/java/frc/robot/robots/SwervyContainer.java
+++ src/test/java/frc/robot/robots/SwervyContainer.java
@@ -10,6 +10,7 @@ import org.northernforce.subsystems.drive.NFRSwerveDrive.NFRSwerveDriveConfigura
 import org.northernforce.subsystems.drive.swerve.NFRSwerveModule;
 import org.northernforce.util.NFRRobotContainer;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -27,10 +28,10 @@ public class SwervyContainer implements NFRRobotContainer
     public SwervyContainer()
     {
         NFRSwerveModule[] modules = new NFRSwerveModule[] {
-            NFRSwerveModule.createMk3Slow("Front Left", 1, 5, 9),
-            NFRSwerveModule.createMk3Slow("Front Right", 2, 6, 10),
-            NFRSwerveModule.createMk3Slow("Back Left", 3, 7, 11),
-            NFRSwerveModule.createMk3Slow("Back Right", 4, 8, 12)
+            NFRSwerveModule.createMk3Slow("Front Left", 1, 5, 9, false),
+            NFRSwerveModule.createMk3Slow("Front Right", 2, 6, 10, true),
+            NFRSwerveModule.createMk3Slow("Back Left", 3, 7, 11, false),
+            NFRSwerveModule.createMk3Slow("Back Right", 4, 8, 12, true)
         };
         NFRSwerveDriveConfiguration driveConfig = new NFRSwerveDriveConfiguration("Swerve Drive");
         Translation2d[] offsets = new Translation2d[] {
@@ -39,48 +40,55 @@ public class SwervyContainer implements NFRRobotContainer
             new Translation2d(-0.581025, 0.581025),
             new Translation2d(-0.581025, -0.581025)
         };
-        drive = new NFRSwerveDrive(driveConfig, modules, offsets, new NFRNavX());
+        NFRNavX gyro = new NFRNavX();
+        Shuffleboard.getTab("Swerve").addNumber("Gyro", () -> gyro.getRotation2d().getDegrees())
+            .withWidget(BuiltInWidgets.kGyro);
+        gyro.reset();
+        drive = new NFRSwerveDrive(driveConfig, modules, offsets, gyro);
         field = new Field2d();
         Shuffleboard.getTab("Swerve").add("Field", field);
         Shuffleboard.getTab("Swerve").add("Front Left", modules[0]);
         Shuffleboard.getTab("Swerve").add("Front Right", modules[1]);
         Shuffleboard.getTab("Swerve").add("Back Left", modules[2]);
         Shuffleboard.getTab("Swerve").add("Back Right", modules[3]);
+        Shuffleboard.getTab("Swerve").addDouble("Front Left Angle", () -> drive.getModules()[0].getRotation().getDegrees())
+            .withWidget(BuiltInWidgets.kGyro);
+        Shuffleboard.getTab("Swerve").addDouble("Front Right Angle", () -> drive.getModules()[1].getRotation().getDegrees())
+            .withWidget(BuiltInWidgets.kGyro);
+        Shuffleboard.getTab("Swerve").addDouble("Back Left Angle", () -> drive.getModules()[2].getRotation().getDegrees())
+            .withWidget(BuiltInWidgets.kGyro);
+        Shuffleboard.getTab("Swerve").addDouble("Back Right Angle", () -> drive.getModules()[3].getRotation().getDegrees())
+            .withWidget(BuiltInWidgets.kGyro);
     }
     @Override
     public void bindOI(GenericHID driverHID, GenericHID manipulatorHID)
     {
+        NFRSwerveModuleSetState[] commands = new NFRSwerveModuleSetState[] {
+            new NFRSwerveModuleSetState(drive.getModules()[0], 0,
+                false),
+            new NFRSwerveModuleSetState(drive.getModules()[1], 0,
+                false),
+            new NFRSwerveModuleSetState(drive.getModules()[2], 0,
+                false),
+            new NFRSwerveModuleSetState(drive.getModules()[3], 0,
+                false)
+        };
         if (driverHID instanceof XboxController && manipulatorHID instanceof XboxController)
         {
             XboxController driverController = (XboxController)driverHID;
-            NFRSwerveModuleSetState[] commands = new NFRSwerveModuleSetState[] {
-                new NFRSwerveModuleSetState(drive.getModules()[0], 0, 0,
-                    false),
-                new NFRSwerveModuleSetState(drive.getModules()[1], 0, 0,
-                    false),
-                new NFRSwerveModuleSetState(drive.getModules()[2], 0, 0,
-                    false),
-                new NFRSwerveModuleSetState(drive.getModules()[3], 0, 0,
-                    false)
-            };
-            Shuffleboard.getTab("Swerve").addNumber("Front Left Target Speed", () -> commands[0].getTargetState() != null ?
-                commands[0].getTargetState().speedMetersPerSecond : 0);
-            Shuffleboard.getTab("Swerve").addNumber("Front Rigt Target Speed", () -> commands[1].getTargetState() != null ?
-                commands[1].getTargetState().speedMetersPerSecond : 0);
-            Shuffleboard.getTab("Swerve").addNumber("Back Left Target Speed", () -> commands[2].getTargetState() != null ?
-                commands[2].getTargetState().speedMetersPerSecond : 0);
-            Shuffleboard.getTab("Swerve").addNumber("Back Right Target Speed", () -> commands[3].getTargetState() != null ?
-                commands[3].getTargetState().speedMetersPerSecond : 0);
-            Shuffleboard.getTab("Swerve").addDouble("Front Left Angle", () -> drive.getModules()[0].getRotation().getDegrees())
-                .withWidget(BuiltInWidgets.kGyro);
-            Shuffleboard.getTab("Swerve").addDouble("Front Right Angle", () -> drive.getModules()[1].getRotation().getDegrees())
-                .withWidget(BuiltInWidgets.kGyro);
-            Shuffleboard.getTab("Swerve").addDouble("Back Left Angle", () -> drive.getModules()[2].getRotation().getDegrees())
-                .withWidget(BuiltInWidgets.kGyro);
-            Shuffleboard.getTab("Swerve").addDouble("Back Right Angle", () -> drive.getModules()[3].getRotation().getDegrees())
-                .withWidget(BuiltInWidgets.kGyro);
-            drive.setDefaultCommand(new NFRSwerveDriveWithJoystick(drive, commands, () -> -driverController.getLeftY(),
-                () -> -driverController.getLeftX(), () -> -driverController.getRightX(), true));
+            drive.setDefaultCommand(new NFRSwerveDriveWithJoystick(drive, commands,
+                () -> -MathUtil.applyDeadband(driverController.getLeftY(), 0.1),
+                () -> -MathUtil.applyDeadband(driverController.getLeftX(), 0.1),
+                () -> -MathUtil.applyDeadband(driverController.getRightX(), 0.1),
+                true, true));
+        }
+        else
+        {
+            drive.setDefaultCommand(new NFRSwerveDriveWithJoystick(drive, commands,
+                () -> MathUtil.applyDeadband(driverHID.getRawAxis(1), 0.1),
+                () -> MathUtil.applyDeadband(driverHID.getRawAxis(0), 0.1),
+                () -> -MathUtil.applyDeadband(driverHID.getRawAxis(4), 0.1),
+                true, true));
         }
     }
     @Override
