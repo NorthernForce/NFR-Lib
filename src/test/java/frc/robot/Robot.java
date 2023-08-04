97e18106cf0232b8e858806e6cbb13c932ba441c
cml1010101 <cml1010101@gmail.com>
2023-08-04 10:26:23 -0400

    cleared button bindings on teleop init

--- src/test/java/frc/robot/Robot.java
+++ src/test/java/frc/robot/Robot.java
@@ -97,6 +97,7 @@ public class Robot extends TimedRobot {
     {
       autonomousCommand.cancel();
     }
+    CommandScheduler.getInstance().getActiveButtonLoop().clear();
     GenericHID driverController;
     if (DriverStation.getJoystickIsXbox(0))
     {
