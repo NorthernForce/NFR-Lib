c8b695635d0242912678cefab971b0f0bc18e53a
cml1010101 <cml1010101@gmail.com>
2023-08-18 15:40:24 -0400

    Add javadocs to NFRSwerveModuleCalibrate.java

--- src/main/java/org/northernforce/commands/NFRSwerveModuleCalibrate.java
+++ src/main/java/org/northernforce/commands/NFRSwerveModuleCalibrate.java
@@ -4,21 +4,37 @@ import org.northernforce.subsystems.drive.swerve.NFRSwerveModule;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * NFRSwerveModuleCalibrate is a command designed to calibrate a single swerve drive module
+ */
 public class NFRSwerveModuleCalibrate extends CommandBase
 {
     protected final NFRSwerveModule module;
     protected final Rotation2d angle;
+    /**
+     * Creates a new NFRSwerveModuleCalibrate.
+     * @param module the module to be calibrated
+     * @param angle the angle to reset to
+     */
     public NFRSwerveModuleCalibrate(NFRSwerveModule module, Rotation2d angle)
     {
         addRequirements(module);
         this.module = module;
         this.angle = angle;
     }
+    /**
+     * Resets the angle of the module.
+     */
     @Override
     public void initialize()
     {
         module.resetAngle(angle);
     }
+    /**
+     * Returns whether the command is finished
+     * @return always true
+     */
     @Override
     public boolean isFinished()
     {
