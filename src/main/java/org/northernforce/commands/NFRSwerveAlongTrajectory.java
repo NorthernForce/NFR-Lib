4fc14f221cf94b14d44996f8a7a33ee78454ed8a
cml1010101 <cml1010101@gmail.com>
2023-08-18 14:59:33 -0400

    Add JavaDocs to NFRSwerveAlongTrajectory

--- src/main/java/org/northernforce/commands/NFRSwerveAlongTrajectory.java
+++ src/main/java/org/northernforce/commands/NFRSwerveAlongTrajectory.java
@@ -10,10 +10,21 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 
+/**
+ * A Command designed to provide a swerve drive with the ability to follow a trajectory.
+ */
 public class NFRSwerveAlongTrajectory extends SwerveControllerCommand
 {
     protected final NFRSwerveDrive drive;
     protected final HolonomicDriveController controller;
+    /**
+     * Creates a new NFRSwerveAlongTrajectory.
+     * @param drive the swerve drive subsystem
+     * @param setStateCommands the commands to be used for setting the states of the swerve modules
+     * @param trajectory the trajectory to follow
+     * @param poseSupplier the pose supplier
+     * @param controller the controller that is to be used
+     */
     public NFRSwerveAlongTrajectory(NFRSwerveDrive drive, NFRSwerveModuleSetState[] setStateCommands, Trajectory trajectory,
         Supplier<Pose2d> poseSupplier, HolonomicDriveController controller)
     {
@@ -26,6 +37,16 @@ public class NFRSwerveAlongTrajectory extends SwerveControllerCommand
         this.drive = drive;
         this.controller = controller;
     }
+    /**
+     * Creates a new NFRSwerveAlongTrajectory.
+     * @param drive the swerve drive subsystem
+     * @param setStateCommands the commands to be used for setting the states of the swerve modules
+     * @param trajectory the trajectory to follow
+     * @param poseSupplier the pose supplier
+     * @param controller the controller that is to be used
+     * @param targetRotation the target rotation that the swerve drive will go to as it follows the translational
+     * components of the trajectory.
+     */
     public NFRSwerveAlongTrajectory(NFRSwerveDrive drive, NFRSwerveModuleSetState[] setStateCommands, Trajectory trajectory,
         Supplier<Pose2d> poseSupplier, HolonomicDriveController controller, Supplier<Rotation2d> targetRotation)
     {
@@ -38,6 +59,10 @@ public class NFRSwerveAlongTrajectory extends SwerveControllerCommand
         this.drive = drive;
         this.controller = controller;
     }
+    /**
+     * Returns whether at the end of the trajectory.
+     * @return whether the estimated time has elapsed AND the controller is at the reference (at the setpoint).
+     */
     @Override
     public boolean isFinished()
     {
