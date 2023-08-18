30f3613626906cddd7523c7589c66f987b0876c6
cml1010101 <cml1010101@gmail.com>
2023-08-18 16:33:10 -0400

    Add child_frame_id field to geometry_msgs/TransformStamped

--- src/main/java/org/northernforce/subsystems/ros/geometry_msgs/TransformStamped.java
+++ src/main/java/org/northernforce/subsystems/ros/geometry_msgs/TransformStamped.java
@@ -11,24 +11,27 @@ import edu.wpi.rail.jrosbridge.messages.geometry.Transform;
 public class TransformStamped extends Message
 {
     public final Header header;
+    public final String child_frame_id;
     public final Transform transform;
     public TransformStamped()
     {
-        this(new Header(), new Transform());
+        this(new Header(), "", new Transform());
     }
-    public TransformStamped(Header header, Transform transform)
+    public TransformStamped(Header header, String child_frame_id, Transform transform)
     {
         super(Json.createObjectBuilder()
             .add("header", header.toJsonObject())
+            .add("child_frame_id", child_frame_id)
             .add("transform", transform.toJsonObject())
             .build(), "geometry_msgs/TransformStamped");
         this.header = header;
+        this.child_frame_id = child_frame_id;
         this.transform = transform;
     }
     @Override
     public TransformStamped clone()
     {
-        return new TransformStamped(header, transform);
+        return new TransformStamped(header, child_frame_id, transform);
     }
     public static TransformStamped fromJsonString(String json)
     {
@@ -42,8 +45,10 @@ public class TransformStamped extends Message
     {
         Header header = object.containsKey("header") ? Header.fromJsonObject(object.getJsonObject("header"))
             : new Header();
+        String child_frame_id = object.containsKey("child_frame_id") ? object.getString("child_frame_id")
+            : "";
         Transform transform = object.containsKey("transform") ?
             Transform.fromJsonObject(object.getJsonObject("transform")) : new Transform();
-        return new TransformStamped(header, transform);
+        return new TransformStamped(header, child_frame_id, transform);
     }
 }
