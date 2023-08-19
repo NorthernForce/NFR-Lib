dfec4221a0f7f4ab9db7b10d2d584ac3fcafa978
cml1010101 <cml1010101@gmail.com>
2023-08-18 20:22:10 -0400

    Remove unnecessary logging

--- src/test/java/frc/robot/robots/SwervyContainer.java
+++ src/test/java/frc/robot/robots/SwervyContainer.java
@@ -84,7 +84,6 @@ public class SwervyContainer implements NFRRobotContainer
     }
     public void recieveDetection(Message message)
     {
-        System.out.println("Recieved detection.");
         PoseWithCovarianceStamped poseStamped = PoseWithCovarianceStamped.fromMessage(message);
         Pose3d pose = new Pose3d(
             new Translation3d(
@@ -101,7 +100,6 @@ public class SwervyContainer implements NFRRobotContainer
                 )
             )
         );
-        System.out.println(poseStamped.header.getStamp().secs + "." + poseStamped.header.getStamp().secs);
         drive.addVisionEstimate(poseStamped.header.getStamp().toSec(), pose.toPose2d());
     }
     @Override
