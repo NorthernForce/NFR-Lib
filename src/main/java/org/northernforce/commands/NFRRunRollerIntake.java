c6f11105747eee94f4216a7249e6bdae7fc96e07
Harlan Haller <64970252+HarlanHaller@users.noreply.github.com>
2023-09-20 17:56:00 -0400

    Feature/#83 roller intake (#140)
    * made base subsystem
    
    * i hast made the comadns!!??!.
    
    * fixid the connEr nit-piks (do we need capital letters)

---
+++ src/main/java/org/northernforce/commands/NFRRunRollerIntake.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.northernforce.commands;
+
+import org.northernforce.subsystems.arm.NFRRollerIntake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** Runs a roller intake */
+public class NFRRunRollerIntake extends CommandBase {
+    protected final NFRRollerIntake intake;
+    protected final double speed;
+    protected final boolean stopOnCommandEnd;
+
+    /**
+     * Sets speed of roller intake (command will not stop motor)
+     * @param intake The roller intake to set speed of 
+     * @param speed The speed to set
+    */
+    public NFRRunRollerIntake(NFRRollerIntake intake, double speed) {
+        this.intake = intake;
+        this.speed = speed;
+        this.stopOnCommandEnd = false;
+    }
+
+    /**
+     * Sets speed of roller intake
+     * @param intake The roller intake to set speed of 
+     * @param speed The speed to set
+     * @param stopOnCommandEnd If true motor will stop on command end
+    */
+    public NFRRunRollerIntake(NFRRollerIntake intake, double speed, boolean stopOnCommandEnd) {
+        this.intake = intake;
+        this.speed = speed;
+        this.stopOnCommandEnd = stopOnCommandEnd;
+    }
+
+    @Override
+    public void initialize() {
+        intake.setSpeed(speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if(stopOnCommandEnd) {
+            intake.setSpeed(0);
+        }
+    }
+    
+}
