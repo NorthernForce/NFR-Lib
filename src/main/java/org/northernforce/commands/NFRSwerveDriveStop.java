102c9ddac3326872cd9a110149f23613da5af91e
cml1010101 <cml1010101@gmail.com>
2023-09-26 19:34:42 -0400

    Add Javadocs to getStopState() and NFRSwerveDriveStop

--- src/main/java/org/northernforce/commands/NFRSwerveDriveStop.java
+++ src/main/java/org/northernforce/commands/NFRSwerveDriveStop.java
@@ -5,11 +5,20 @@ import org.northernforce.subsystems.drive.NFRSwerveDrive;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * Points the wheels inwards, effectively stopping the swerve drive.
+ */
 public class NFRSwerveDriveStop extends CommandBase
 {
     protected final NFRSwerveDrive drive;
     protected final NFRSwerveModuleSetState[] setStateCommands;
     protected final boolean optimize;
+    /**
+     * Creates a new NFRSwerveDriveStop
+     * @param drive the swerve drive to stop
+     * @param setStateCommands the set state commands for the modules
+     * @param optimize whether to optimize (should be true)
+     */
     public NFRSwerveDriveStop(NFRSwerveDrive drive, NFRSwerveModuleSetState[] setStateCommands, boolean optimize)
     {
         addRequirements(drive);
@@ -29,6 +38,14 @@ public class NFRSwerveDriveStop extends CommandBase
         {
             setStateCommands[i].setTargetState(optimize ? SwerveModuleState.optimize(states[i],
                 drive.getModules()[i].getRotation()) : states[i]);
+        }
+    }
+    @Override
+    public void end(boolean interrupted)
+    {
+        for (var command : setStateCommands)
+        {
+            command.cancel();
         }
     }
 }
