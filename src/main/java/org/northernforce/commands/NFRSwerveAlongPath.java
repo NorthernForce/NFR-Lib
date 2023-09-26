4d8dffa9de1e8fefdcb157eb6571a5bf8e82f2c2
cml1010101 <cml1010101@gmail.com>
2023-09-26 19:32:18 -0400

    Add Javadocs to NFRSwerveAlongPath

--- src/main/java/org/northernforce/commands/NFRSwerveAlongPath.java
+++ src/main/java/org/northernforce/commands/NFRSwerveAlongPath.java
@@ -8,12 +8,22 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 
+/**
+ * The NFR
+ */
 public class NFRSwerveAlongPath extends CommandBase
 {
     protected final NFRSwerveDrive drive;
     protected final SwerveControllerCommand command;
     protected final NFRSwerveModuleSetState[] setStateCommands;
     protected final HolonomicDriveController controller;
+    /**
+     * Creates a new NFRSwerveAlongPath
+     * @param drive the drivetrain
+     * @param trajectory the WPILib trajectory to follow
+     * @param controller the holonomic drive controller
+     * @param setStateCommands the commands for the swerve modules
+     */
     public NFRSwerveAlongPath(NFRSwerveDrive drive, Trajectory trajectory, HolonomicDriveController controller,
         NFRSwerveModuleSetState[] setStateCommands)
     {
@@ -55,6 +65,10 @@ public class NFRSwerveAlongPath extends CommandBase
     {
         return command.isFinished() && controller.atReference();
     }
+    /**
+     * Sets the module states
+     * @param states the states to go to
+     */
     public void setModuleStates(SwerveModuleState[] states)
     {
         for (int i = 0; i < states.length; i++)
